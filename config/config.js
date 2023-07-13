cd42b474777f7e5f931d02447a8da72f6befc0f7
Xhydracore <sutrisnodeyndrawan@gmail.com>
2023-07-13 12:41:33 +0700

    config set

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const dotenv = require("dotenv");
-dotenv.config({ path: "config.env" });
+dotenv.config({ path: ".env" });
 module.exports = {
   development: {
     username: 'Deyndrawan',
